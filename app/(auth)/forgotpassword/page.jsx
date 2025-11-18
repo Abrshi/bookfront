bc1f2b3c75fbc2e88f9c0221a035abc18facf243
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2025-11-18 03:37:13 -0800

    deploing fix remov forgot passeored

--- app/(auth)/forgotpassword/page.jsx
+++ app/(auth)/forgotpassword/page.jsx
@@ -1,8 +1,9 @@
-
 import React from 'react'
-import Forgot from '@/components/Auth/Forgot'
 
-export default function Page() {
-  return <Forgot />
+function page() {
+  return (
+    <div>page</div>
+  )
 }
-  
+
+export default page
