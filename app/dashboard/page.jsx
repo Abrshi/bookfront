af4057a5a01db4c9b2e98fa6ff2b5331179d1359
Abreham Yeshitla <abrhamyeshitla990@gmail.com>
2025-12-01 06:12:07 -0800

    ai book review

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Book from '@/components/Home/books/books'
 function page() {
   return (
-    <div className='bg-black w-[100vw] h-[100vh] text-white'>
+    <div className='bg-black w-[99vw] h-[100vh] text-white'>
       <Book />
     </div>
   )
